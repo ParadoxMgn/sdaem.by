8d09e14217c011452d9a73dd54acd55f4c3a9265
Eugene <paradox456@mail.ru>
2022-12-06 23:58:41 +0300

    fix routs file

--- src/components/Routs/index.tsx
+++ src/components/Routs/index.tsx
@@ -1,21 +1,20 @@
 import { FC } from 'react';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import list from './list';
 
 
 const Routs: FC = () => {
   
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        {/* @ts-ignore */}
         {
           list.map(({ path, element }) =>
             <Route path={path} element={element} key={path} />
           )
         }
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
